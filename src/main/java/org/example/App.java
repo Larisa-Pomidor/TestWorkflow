5a384e1f72b1a5ca6157b4d466632a77d1b11775
Larisa Pomidor <nikakinikakin@gmail.com>
2023-08-20 18:31:39 +0300

    Test commit api

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import org.kohsuke.github.*;
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World4!" );
+        String authToken = "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";
+        String owner = "Larisa-Pomidor";
+        String repoName = "TestWorkflow";
+        String commitSha = "20ec40c0ddb0fdb26b41574e3c9f7856211dce86";
+
+        try {
+            GitHub github = new GitHubBuilder().withOAuthToken(authToken).build();
+            GHRepository repository = github.getRepository(owner + "/" + repoName);
+
+            GHCommit commit = repository.getCommit(commitSha);
+            PagedIterable<GHCommitStatus> commitStatuses = commit.listStatuses();
+
+            for (GHCommitStatus status : commitStatuses) {
+                System.out.println("Context: " + status.getContext());
+                System.out.println("State: " + status.getState());
+                System.out.println("Description: " + status.getDescription());
+                System.out.println("-------------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
